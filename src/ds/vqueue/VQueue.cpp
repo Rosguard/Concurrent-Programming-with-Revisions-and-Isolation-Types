6d98c53e001956a8734c843c7df3611ad7ff0add
neko2a <nickita.kaminsckij@yandex.ru>
2021-12-24 13:15:13 +0700

    Added VQueue

--- src/ds/vqueue/VQueue.cpp
+++ src/ds/vqueue/VQueue.cpp
@@ -1,1 +1,21 @@
 #include "../../../include/VQueue.h"
+
+template <class T> void VQueue<T>::push(const T &value)
+{
+	this->get().push(value);
+}
+
+template <class T> void VQueue<T>::pop()
+{
+	this->get().pop();
+}
+
+template <class T> T &VQueue<T>::front()
+{
+	return this->get().front();
+}
+
+template <class T> size_t VQueue<T>::size() const
+{
+	return this->get().size();
+}
