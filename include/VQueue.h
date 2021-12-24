e62775d6a058763d24e2fd1c12420f35b785dafe
neko2a <nickita.kaminsckij@yandex.ru>
2021-12-24 13:15:13 +0700

    Added VQueue

--- include/VQueue.h
+++ include/VQueue.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include "ds/VDataStructure.h"
+#include <queue>
+
+template <class T> class VQueue : VDataStructure<std::queue<T> > {
+    public:
+	void push(const T &value);
+	void pop();
+	T &front();
+	[[nodiscard]] size_t size() const;
+};
